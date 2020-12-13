5954d28c4de1a7f2af78ef5949a67a1d16355473
Blaz Orazem <blaz@orazem.si>
2020-12-13 19:52:04 +0100

    Remote commands setup

---
+++ config/remote.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Remote connections
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for the remote server connections.
+    |
+    */
+
+    'connections' => [
+        'production' => [
+            'key'              => env('REMOTE_PRODUCTION_KEY'),
+            'path'             => env('REMOTE_PRODUCTION_PATH'),
+            'branch'           => env('REMOTE_PRODUCTION_BRANCH', 'prod'),
+            'host'             => env('REMOTE_PRODUCTION_HOST'),
+            'username'         => env('REMOTE_PRODUCTION_USERNAME'),
+            'keyphrase'        => env('REMOTE_PRODUCTION_KEYPHRASE', ''),
+            'timeout'          => 600,
+            'permissions'      => [
+                'root_user'   => env('REMOTE_PRODUCTION_ROOT_USER'),
+                'www_user'    => env('REMOTE_PRODUCTION_WWW_USER'),
+                'www_folders' => env('REMOTE_PRODUCTION_WWW_FOLDERS'),
+            ],
+        ],
+    ],
+
+];
