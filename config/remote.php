2489cd2e55298df99475726ad28e65fc067ec42f
Blaz Orazem <blaz@orazem.si>
2020-12-13 23:10:20 +0100

    Database pull settings

--- config/remote.php
+++ config/remote.php
@@ -13,18 +13,27 @@ return [
 
     'connections' => [
         'production' => [
-            'key'              => env('REMOTE_PRODUCTION_KEY'),
-            'path'             => env('REMOTE_PRODUCTION_PATH'),
-            'branch'           => env('REMOTE_PRODUCTION_BRANCH', 'prod'),
+            'key'         => env('REMOTE_PRODUCTION_KEY'),
+            'path'        => env('REMOTE_PRODUCTION_PATH'),
+            'branch'      => env('REMOTE_PRODUCTION_BRANCH', 'prod'),
             // 'master_branch'    => env('REMOTE_PRODUCTION_MASTER_BRANCH'),
-            'host'             => env('REMOTE_PRODUCTION_HOST'),
-            'username'         => env('REMOTE_PRODUCTION_USERNAME'),
-            'keyphrase'        => env('REMOTE_PRODUCTION_KEYPHRASE', ''),
-            'timeout'          => 600,
-            'permissions'      => [
+            'host'        => env('REMOTE_PRODUCTION_HOST'),
+            'username'    => env('REMOTE_PRODUCTION_USERNAME'),
+            'keyphrase'   => env('REMOTE_PRODUCTION_KEYPHRASE', ''),
+            'timeout'     => 600,
+            'permissions' => [
                 'root_user'   => env('REMOTE_PRODUCTION_ROOT_USER'),
                 'www_user'    => env('REMOTE_PRODUCTION_WWW_USER'),
                 'www_folders' => env('REMOTE_PRODUCTION_WWW_FOLDERS'),
+            ],
+            'database' => [
+                'name'     => env('REMOTE_DB_DATABASE'),
+                'username' => env('REMOTE_DB_USERNAME'),
+                'password' => env('REMOTE_DB_PASSWORD'),
+                'tables'   => [
+                    'rainlab_blog_posts',
+                    'rainlab_blog_categories',
+                ],
             ],
         ],
     ],
